7e9f0b0f752d98ec4d540a299f2e66bdc7f271e8
aney <aney@aneyui-MacBook-Pro.local>
2017-01-13 04:05:26 +0900

    Added PhotoDemo

---
+++ iOS10 Photo Demo/iOS10 Photo Demo/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  iOS10 Photo Demo
+//
+//  Created by aney on 2017. 1. 13..
+//  Copyright © 2017년 aney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+        } else {
+            print("imagePickerController : There was a problem getting the image")
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func importImage(_ sender: Any) {
+        
+        let imagePickerController = UIImagePickerController()
+        
+        imagePickerController.delegate = self
+        
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        imagePickerController.allowsEditing = false
+        
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
